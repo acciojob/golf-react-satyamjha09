aaa8e1a03e3bac912c541bd9e8456c041d929cbf
satyamjha <satyamjhaactor07@gmail.com>
2025-02-07 18:43:06 +0530

    Done project

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,53 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" },
     };
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+    this.renderChoice = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return <button onClick={this.buttonClickHandler}>Start</button>;
     }
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {
+    if (event.keyCode === 39 && this.state.renderBall) {
+      // Move ball 5px right on Right Arrow key press
+      this.setState((prevState) => ({
+        posi: prevState.posi + 5,
+        ballPosition: { left: `${prevState.posi + 5}px` },
+      }));
+    }
+  }
+
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
